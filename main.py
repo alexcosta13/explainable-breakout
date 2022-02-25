913ccd06e10bcbdd2d022584db848bb724604606
alexcosta13 <39431985+alexcosta13@users.noreply.github.com>
2022-02-25 11:01:58 +0100

    feat: render environment

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
+import time
+
 import gym
 
 from agent import Agent
+
+RENDER = True
 
 
 def main():
     env = gym.make('Breakout-v4')
     env.reset()
     agent = Agent(env.action_space.n)
-    for _ in range(100):
-        new_state, reward, is_done, info = env.step(agent.next_action())
+    is_done = False
+    while not is_done:
+        action = agent.next_action()
+        new_state, reward, is_done, info = env.step(action)
         print(info)
+        if RENDER:
+            env.render()
+            time.sleep(0.1)
 
 
 if __name__ == "__main__":
